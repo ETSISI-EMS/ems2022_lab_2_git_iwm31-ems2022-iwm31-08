4463d23f6af83027079a8ccde1f57cab1f76305f
UPM <zadkiel.zabaleta.medina@alumnos.upm.es>
2022-03-31 21:41:03 +0200

    ahora si

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
  	public static void main(String[] args) {
  	SpringApplication.run(Application.class, args);
-<<<<<<< HEAD
-//jvbscjvbsu fuhj eoj e
-=======
-//Comentario de alex
->>>>>>> 87dd973d5f16d3263f5f0311465b175262c94d49
  }
 
 }
